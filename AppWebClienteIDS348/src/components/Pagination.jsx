ba0ef2372078761a823574c3ec2ec40941ef3d9a
GleidyEspinal <1104225@est.intec.edu.do>
2023-10-12 17:11:25 -0400

    Detalles en pagination

--- AppWebClienteIDS348/src/components/Pagination.jsx
+++ AppWebClienteIDS348/src/components/Pagination.jsx
@@ -1,33 +1,26 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Paginator } from 'primereact/paginator';
+import 'primereact/resources/primereact.min.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
 
-const Pagination = () => {
-  return (
-    <div aria-label="Page navigation example" className="flex justify-center">
-      <ul className="inline-flex -space-x-px text-sm">
-        <li>
-          <a href="#" className="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
-        </li>
-        <li>
-          <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
-        </li>
-        <li>
-          <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
-        </li>
-        <li>
-          <a href="#" aria-current="page" className="flex items-center justify-center px-3 h-8 text-purple-600 border border-gray-300 bg-purple-50 hover:bg-purple-100 hover-text-purple-700 dark:border-gray-700 dark-bg-gray-700 dark-text-white">3</a>
-        </li>
-        <li>
-          <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover-bg-gray-100 hover-text-gray-700 dark-bg-gray-800 dark-border-gray-700 dark-text-gray-400 dark-hover-bg-gray-700 dark-hover-text-white">4</a>
-        </li>
-        <li>
-          <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover-bg-gray-100 hover-text-gray-700 dark-bg-gray-800 dark-border-gray-700 dark-text-gray-400 dark-hover-bg-gray-700 dark-hover-text-white">5</a>
-        </li>
-        <li>
-          <a href="#" className="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover-bg-gray-100 hover-text-gray-700 dark-bg-gray-800 dark-border-gray-700 dark-text-gray-400 dark-hover-bg-gray-700 dark-hover-text-white">Next</a>
-        </li>
-      </ul>
-    </div>
-  );
-};
 
-export default Pagination;
+
+export default function Pagination() {
+    const [first, setFirst] = useState(0);
+    const [rows, setRows] = useState(10);
+
+    const onPageChange = (event) => {
+        setFirst(event.first);
+        setRows(event.rows);
+    };
+
+    return (
+        <div className="">
+            <Paginator first={first}
+             rows={rows} 
+            totalRecords={120}  
+            onPageChange={onPageChange} />
+        </div>
+    );
+}
+        
