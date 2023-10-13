6e73e644f5297d327309599728529130ac430089
GleidyEspinal <1104225@est.intec.edu.do>
2023-10-13 05:02:59 -0400

    detalles

--- AppWebClienteIDS348/src/pages/Poduct.jsx
+++ AppWebClienteIDS348/src/pages/Poduct.jsx
@@ -1,20 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { ProductDetails } from '@/services/productoService'; // Asegúrate de importar la función desde el archivo correcto
+import { useLocation } from 'react-router-dom';
+import ProductPhotos from '@/components/ProductPhotos';
 
 
 const Product = () => {
-  const [productData, setProductData] = useState(null);
+  const location = useLocation(); // Utiliza useLocation para obtener la ubicación actual
+  const idParam = new URLSearchParams(location.search).get('id'); // Obtiene el valor del parámetro id
+  const [dataProduct, setProductData] = useState(null);
 
   useEffect(() => {
-    const currentURL = window.location.pathname;
-    const urlParts = currentURL.split('/');
-    const productId = urlParts[urlParts.length - 1];
-
+  
     const fetchProductDetails = async () => {
       try {
-        const data = await ProductDetails(productId);
-        setProductData(data);
-      } catch (error) {
+        console.log("ENTRE USEFECT DETALLES PRODUCTOOOO: " + idParam);
+        let response;
+        if(idParam == undefined || idParam == ""){
+          response = await ProductoPaginacion(pag+1);
+      }
+      else{
+        console.log(dataProduct);  
+        response = await ProductDetails(idParam); // Llama a la función para obtener los detalles del producto
+        const dataProduct = response.data; 
+        console.log(dataProduct);
+        ProductDetails(idParam);
+      }
+   
+
+    } catch (error) {
         console.error('Error al obtener detalles del producto:', error);
       }
     };
@@ -23,11 +36,6 @@ const Product = () => {
   }, []);
 
 
-  if (!productData) {
-    // Puedes mostrar un indicador de carga aquí si los datos aún no se han cargado
-    return <div>Cargando...</div>;
-  }
-
   // Una vez que los datos estén disponibles, puedes renderizar tu componente Product con los datos
   return (
    
@@ -35,27 +43,29 @@ const Product = () => {
       <div>
       <ProductPhotos />
       </div>
+     
       <div className="ml-20 mt-50">
-        <h1 className="text-6xl">{productData.nombre}</h1>
+        <h1 className="text-6xl">{dataProduct.nombre}</h1>
         <div className="border-b border-gray-300 pb-2 mb-4">
       {/* Esta es la línea debajo del nombre del producto */}
       </div>
-        <p className="text-3xl text-green-500">{productData.availability}</p>
+        <p className="text-3xl text-green-500">{dataProduct.availability}</p>
         <p className="text-2xl">Precio:</p>
-        <p className="text-2xl text-red-500">$/ {productData.precio}</p>
+        <p className="text-2xl text-red-500">$/ {dataProduct.precio}</p>
         <div class="flex flex-row">
         <div class="basis-1/4">
         </div>
         <p className="text-2xl">Tamaño: </p>
+        <p className="text-2xl">{dataProduct.size} </p>
         </div>
-        <p className="text-2xl text-red-500">$/ {productData.nombreEstadoProductos}</p>
+        <p className="text-2xl text-red-500">$/ {dataProduct.nombreEstadoProductos}</p>
 
         <div className='basis-1/4 mx-2' >
         <p className="text-2xl">Color: </p> 
-        <div className="w-6 h-6 rounded-full border border-black" style={{ backgroundColor: "#000000" }}></div>
+        <div className="w-6 h-6 rounded-full border border-black" style={{ dataProduct: "#000000" }}></div>
         </div>
 
-        <p className="text-2xl">{productData.descripcion}</p>
+        <p className="text-2xl">{dataProduct.descripcion}</p>
         <div  className="border border-gray-300 p-4 rounded">
           <p>{productData.Size}</p>
         </div>
@@ -71,6 +81,7 @@ const Product = () => {
       </div>
       </div>
       </div>
+      
   );
 };
 
--- AppWebClienteIDS348/src/services/productoService.js
+++ AppWebClienteIDS348/src/services/productoService.js
@@ -55,11 +55,17 @@ export const totalProductos = async () => {
 // Define una función que obtenga los detalles del producto por su ID
 export const ProductDetails = async (productId) => {
   try {
-    // Realiza una solicitud a tu API o base de datos para obtener los detalles del producto
-    const response = await axios.get(`/api/Product/${productId}`); // Ajusta la URL según tu estructura
+    console.log("ENTRE A PRODUCTO. id: " + productId)
 
-    // Si la solicitud se realiza con éxito, devuelve los detalles del producto
-    return response.data;
+    // Realiza una solicitud a tu API o base de datos para obtener los detalles del producto
+    const response = await axios.get(BASE_URL + `ProductoEspecifico/${productId}`,{
+      headers: {
+        "Content-Type": "application/json", // Establece el tipo de contenido como JSON
+      },
+  });      
+  const respuesta = new ResponseAPI(response.status, response.data, response.statusText);
+  return respuesta;
+  
   } catch (error) {
     // En caso de un error en la solicitud, maneja el error adecuadamente
     console.error('Error al obtener detalles del producto:', error);
